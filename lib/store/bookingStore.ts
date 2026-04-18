f90da582cf06afb8fdea3887b8c7dc899cb3e7ff
mishokkartoshki <misha@moskalov.com>
2026-04-18 21:19:18 +0200

    declare .css && finish schemas && store

---
+++ lib/store/bookingStore.ts
@@ -0,0 +1,28 @@
+import { Booking } from "@/types/booking";
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
+
+type BookingDraftStore = {
+  draft: Booking;
+  setDraft: (book: Booking) => void;
+  clearDraft: () => void;
+};
+
+const initialDraft: Booking = {
+  name: "",
+  email: "",
+};
+
+export const useBookingStore = create<BookingDraftStore>()(
+  persist(
+    (set) => ({
+      draft: initialDraft,
+      setDraft: (book) => set(() => ({ draft: book })),
+      clearDraft: () => set(() => ({ draft: initialDraft })),
+    }),
+    {
+      name: "booking-draft",
+      partialize: (state) => ({ draft: state.draft }),
+    },
+  ),
+);
---
+++ types/camperCharacteristics.ts
@@ -0,0 +1,30 @@
+export enum CamperForm {
+  CamperForm,
+  "panel_van",
+  "integrated",
+  "semi_integrated",
+}
+
+export enum CamperTransmission {
+  "automatic",
+  "manual",
+}
+
+export enum CamperEngine {
+  "diesel",
+  "petrol",
+  "hybrid",
+  "electric",
+}
+
+export enum CamperAmenities {
+  "ac",
+  "bathroom",
+  "kitchen",
+  "tv",
+  "radio",
+  "refrigerator",
+  "microwave",
+  "gas",
+  "water",
+}
